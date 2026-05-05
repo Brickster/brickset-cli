dca4192ce4b8e662cba1c39d14c2074b7a7e2b1b
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2026-05-04 20:10:36 -0500

    build(nox): delegate test to tests

--- noxfile.py
+++ noxfile.py
@@ -9,10 +9,14 @@ def lint(session):
     session.run("flake8", ".", "--count", "--select=E9,F63,F7,F82", "--show-source", "--statistics")
     session.run("flake8", ".", "--count", "--exit-zero", "--max-complexity=10", "--statistics")
 
-
 @nox.session(python=PYTHON)
 def tests(session):
     session.install("-r", "requirements-test.txt")
     session.run("coverage", "run", "-m", "pytest", "-v", *session.posargs)
     session.run("coverage", "lcov", "-o", "coverage.lcov")
     session.run("coverage", "report")
+
+
+@nox.session(python=PYTHON, default=False)
+def test(session):
+    tests(session)
